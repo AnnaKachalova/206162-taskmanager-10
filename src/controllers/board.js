64c34cc6bce7a75330e6fcd208b54225aa52c745
Anna Kachalova <kachalova@uchi.ru>
2019-12-25 21:22:28 +0300

    Учебный проект: разделяй и властвуй (Часть 2)

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -2,7 +2,7 @@ import MoreButtonComponent from '../components/load-more-button.js';
 import TaskComponent from '../components/task.js';
 import TaskEditComponent from '../components/edit-task.js';
 import TasksComponent from '../components/tasks.js';
-import SortComponent from '../components/sort.js';
+import SortComponent, {SortType} from '../components/sort.js';
 import NoTasksComponent from '../components/no-tasks.js';
 import {render, remove, replace, RenderPosition} from '../utils/render.js';
 
@@ -10,7 +10,7 @@ const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 const renderTask = (taskListElement, task) => {
-  const onEscKeyDown = (evt) => {
+  const onEscKeyDown = evt => {
     const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
 
     if (isEscKey) {
@@ -35,17 +35,38 @@ const renderTask = (taskListElement, task) => {
 
   render(taskListElement, taskComponent, RenderPosition.BEFOREEND);
 };
+const renderTasks = (taskListElement, tasks) => {
+  tasks.forEach((task) => {
+    renderTask(taskListElement, task);
+  });
+};
 
 export default class BoardController {
   constructor(container) {
     this._container = container;
 
     this._noTasksComponent = new NoTasksComponent();
-    this._sortComponent = new SortComponent();
     this._tasksComponent = new TasksComponent();
     this._moreButtonComponent = new MoreButtonComponent();
+    this._sortComponent = new SortComponent();
   }
   render(tasks) {
+    const renderLoadMoreButton = () => {
+      let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+      if (showingTasksCount >= tasks.length) {
+        return;
+      }
+      render(container, this._moreButtonComponent, RenderPosition.BEFOREEND);
+      this._moreButtonComponent.setClickHandler(() => {
+        const prevTasksCount = showingTasksCount;
+        showingTasksCount += SHOWING_TASKS_COUNT_BY_BUTTON;
+
+        tasks.slice(prevTasksCount, showingTasksCount).forEach(task => renderTask(taskListElement, task));
+        if (showingTasksCount >= tasks.length) {
+          remove(this._loadMoreButtonComponent);
+        }
+      });
+    };
     const container = this._container.getElement();
     const isAllTasksArchived = tasks.every((task) => task.isArchive);
 
@@ -60,16 +81,27 @@ export default class BoardController {
       tasks.slice(0, showingTasksCount).forEach((task) => {
         renderTask(taskListElement, task);
       });
+      renderTasks(taskListElement, tasks.slice(0, showingTasksCount));
+      renderLoadMoreButton();
+      this._sortComponent.setSortTypeChangeHandler(sortType => {
+        let sortedTasks = [];
 
-      render(container, this._moreButtonComponent, RenderPosition.BEFOREEND);
-
-      this._moreButtonComponent.setClickHandler(() => {
-        const prevTasksCount = showingTasksCount;
-        showingTasksCount += SHOWING_TASKS_COUNT_BY_BUTTON;
-
-        tasks.slice(prevTasksCount, showingTasksCount).forEach((task) => renderTask(taskListElement, task));
-
-        if (showingTasksCount >= tasks.length) {
+        switch (sortType) {
+          case SortType.DATE_UP:
+            sortedTasks = tasks.slice().sort((a, b) => a.dueDate - b.dueDate);
+            break;
+          case SortType.DATE_DOWN:
+            sortedTasks = tasks.slice().sort((a, b) => b.dueDate - a.dueDate);
+            break;
+          case SortType.DEFAULT:
+            sortedTasks = tasks.slice(0, showingTasksCount);
+            break;
+        }
+        taskListElement.innerHTML = ``;
+        renderTasks(taskListElement, sortedTasks);
+        if (sortType === SortType.DEFAULT) {
+          renderLoadMoreButton();
+        } else {
           remove(this._moreButtonComponent);
         }
       });
